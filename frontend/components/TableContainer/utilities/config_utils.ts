be6ad2d4121c32000feb5e667e01d0123629d092
Jacob Shandling <jacob@fleetdm.com>
2024-04-26 13:22:15 -0700

    Update select all checkbox behavior to exclude inherited queries

---
+++ frontend/components/TableContainer/utilities/config_utils.ts
@@ -0,0 +1,61 @@
+// from https://stackoverflow.com/a/68213902/15458245
+
+// Note: This is a lite solution and does NOT include ability to
+// "Select all across pages" nor to disable the header checkbox
+
+import { HeaderProps, Row } from "react-table";
+
+interface GetConditionalSelectHeaderCheckboxProps {
+  /** react-table header props */
+  headerProps: React.PropsWithChildren<HeaderProps<any>>;
+  /** A function defining which rows are selectable */
+  checkIfRowIsSelectable: (row: Row<any>) => boolean;
+}
+
+export const getConditionalSelectHeaderCheckboxProps = ({
+  headerProps,
+  checkIfRowIsSelectable,
+}: GetConditionalSelectHeaderCheckboxProps) => {
+  // Define if the checkbox should show as checked or indeterminate
+  const checkIfAllSelectableRowsSelected = (rows: Row<any>[]) =>
+    rows.filter(checkIfRowIsSelectable).every((row) => row.isSelected);
+  // Note: This is where we would include disabled logic if we needed it
+
+  // Naming matches react-table v7 https://react-table-v7-docs.netlify.app/docs/api/useRowSelect#instance-properties
+  // getToggleAllPageRowsSelectedProps: Function(props) => props
+  const allSelectableRowsSelected = checkIfAllSelectableRowsSelected(
+    headerProps.rows
+  );
+  const indeterminate =
+    !allSelectableRowsSelected &&
+    headerProps.rows.some((row) => row.isSelected);
+
+  const onChange = () => {
+    // If all selectable rows are already selected, deselect all selectable rows on the page
+    if (checkIfAllSelectableRowsSelected(headerProps.rows)) {
+      headerProps.rows.forEach((row) => {
+        headerProps.toggleRowSelected(row.id, false);
+      });
+    } else {
+      // Otherwise select every selectable row on the page
+      headerProps.page.forEach((row) => {
+        const rowChecked = checkIfRowIsSelectable(row);
+        headerProps.toggleRowSelected(row.id, rowChecked);
+      });
+    }
+  };
+
+  // Usual checkbox props
+  const checkboxProps = headerProps.getToggleAllRowsSelectedProps();
+
+  // For conditional select, we override value, indeterminate and onChange
+  return {
+    ...checkboxProps,
+    value: allSelectableRowsSelected,
+    indeterminate,
+    onChange,
+    // disabled, // Not included
+  };
+};
+
+export default { getConditionalSelectHeaderCheckboxProps };
